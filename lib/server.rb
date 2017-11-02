af9d6c0b67c61e873e2ba0fe3cf273afc9215cf6
David Maurer <davemaurermail@gmail.com>
2017-11-01 20:31:11 -0600

    get /hello and root path working

--- lib/server.rb
+++ lib/server.rb
@@ -12,20 +12,31 @@ class Server
     while @listening do
       client = @server.accept
       handle_request(client)
-
-      body = "<html><head></head><body><h1>Hello World, Count: #{counter}</h1></body></html>"
-      client.puts ["http/1.1 200 ok",
-        "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-        "server: ruby",
-        "content-type: text/html; charset=iso-8859-1",
-        "content-length: 55\r\n\r\n"].join("\r\n")
-      client.puts body
     end
   end
 
   def handle_request(client)
     build_request_headers(client)
-    
+    client.puts response_headers
+    respond_with_hello(client) if @headers[:verb] == "GET" && @headers[:path] == "/hello"
+    respond_with_root_info(client) if @headers[:verb] == "GET" && @headers[:path] == "/"
+  end
+
+  def response_headers
+    ["http/1.1 200 ok",
+     "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+     "server: ruby",
+     "content-type: text/html; charset=iso-8859-1",
+     "content-length: 55\r\n\r\n"].join("\r\n")
+  end
+
+  def respond_with_root_info(client)
+    client.puts debugging_output
+  end
+
+  def respond_with_hello(client)
+    @hello_counter += 1
+    client.puts "<html><head></head><body><h1>Hello World, Count: #{@hello_counter}</h1></body></html>"
   end
 
   def parse_request(client)
@@ -35,7 +46,7 @@ class Server
       request_lines << line
       line = client.gets
     end
-    request_lines.map { |line| line.chomp.split(" ") }
+    request_lines.map { |l| l.chomp.split(" ") }
   end
 
   def debugging_output
